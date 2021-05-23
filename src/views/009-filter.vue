e781f7edb8e34d4f8118dcf9c63a04a5da79090a
ZGZ <301082566@qq.com>
2021-05-23 14:52:43 +0800

    @zgz 添加数据驱动的过滤器

---
+++ src/views/009-filter.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class='c-0'>
+    <div class='category' v-for="category in data" :key="category.name">
+      <div>{{category.name}}</div>
+
+      <div class="items">
+        <div class='item' v-for="item in category.items" :key="item.name">
+          <div :style="{background:item.value?'red':'white'} " @click="itemClick(category, item)">{{item.name}}</div>
+        </div>
+      </div>
+
+      <el-button @click="test">test</el-button>
+
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  components: {},
+  props: [],
+  data() {
+    return {
+      data: [
+        {
+          name: '类别1',
+          type: 'enum',
+          items: [
+            {
+              name: '111',
+              default: true,
+              value: true,
+            },
+             {
+              name: '222',
+              default: true,
+              value: true,
+            }
+          ]
+        },
+         {
+          name: '类别2',
+          type: 'enum',
+          items: [
+            {
+              name: '33',
+              default: false,
+              value: false,
+            },
+             {
+              name: '44',
+              default: false,
+              value: false,
+            }
+          ]
+        }
+      ]
+    };
+  },
+  computed: {},
+  watch: {},
+  created() {
+  },
+  mounted() {
+  },
+  methods: {
+    itemClick(category, item) {
+      console.error(category.name, item.name)
+      item.value = !item.value
+    },
+    test() {
+      this.data[0].items[0].value = !this.data[0].items[0].value
+    }
+  },
+};
+</script>
+<style lang="scss" scoped>
+
+.category {
+  border: solid red 1px;
+
+  display: flex;
+  flex-direction: column;
+
+  .items {
+    display: flex;
+    flex-direction: row;
+
+    .item {
+      border: solid red 1px;
+      margin: 10px;
+    }
+    
+  }
+}
+</style>
